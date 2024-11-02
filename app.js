a290881e3eaa66ce51d7f8b995bdd2ecbf71c3de
Satyaaaaaaaaa <163241295+Satyaaaaaaaaa@users.noreply.github.com>
2024-11-02 11:10:43 +0530

    cors issue

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "https://visionary-pixie-fb4d56.netlify.app"
+}))
 
 app.use(authRouter);
 
